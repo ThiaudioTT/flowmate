1b982a3477313ab0a8e495ce1a0231eaa7a7f0c3
ThiaudioTT <thiago.sky25@gmail.com>
2024-01-12 19:43:08 -0300

    feat: adds timer

--- src/app/components/clock/clock.component.ts
+++ src/app/components/clock/clock.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./clock.component.scss']
 })
 export class ClockComponent {
+  timerDisplay = '00:00';
+  seconds = 0;
 
+
+  /**
+   * Start the timer
+   */
+  startTimer() {
+    setInterval(() => {
+      this.seconds++;
+      this.timerDisplay = this.formatTime(this.seconds);
+    }, 1000);
+  }
+
+
+  /**
+   * Format the time
+   * @param seconds
+   */
+  formatTime(seconds: number): string {
+
+    const minutes = Math.floor(seconds / 60);
+    const sec = seconds % 60;
+
+
+    let formatString = (time: number) => {
+      return time < 10 ? `0${time}` : time;
+    }
+
+    return `${formatString(minutes)}:${formatString(sec)}`;
+  }
+
+
+  /**
+   * Helper function to check if the clock is running
+   * @returns boolean
+   */
+  isClockRunning() {
+    return this.seconds > 0;
+  }
 }
